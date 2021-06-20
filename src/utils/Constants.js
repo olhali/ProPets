4cb33219bcfbd7534d06485ddc7d8eee951c9d10
Olha Linetska <happiest4265@gmail.com>
2021-06-20 22:30:12 +0300

    Production urls

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -10,84 +10,30 @@ export const WALKING = 'walking';
 export const FOSTERING = 'fostering';
 export const VET_HELP = 'vetHelp';
 export const CARE = 'care';
-/*export const FAVORITES = 'favorites';*/
+
 export const hostnameExternal = 'https://84.228.171.29:3082';
-export const hostnamelocalHome = 'https://192.168.31.100:3082';
 
-export const urlLogin = 'https://192.168.31.100:3082/auth/api/auth/signin';
-export const urlRegistration = 'https://192.168.31.100:3082/auth/api/auth/signup';
-export const profileName = 'https://192.168.31.100:3082/auth/api/auth/getProfileName';
-export const urlResetPassword = 'https://192.168.31.100:3082/auth/api/auth/user/resetPassword';
-export const urlChangePassword = 'https://192.168.31.100:3082/auth/api/auth/user/changePassword';
-/*export const localChangePassword = 'https://192.168.31.25:8080/api/auth/user/changePassword';*/
-export const authenticate = 'https://192.168.31.100:3082/auth/api/auth/authenticate';
+export const urlLogin = 'https://84.228.171.29:3082/auth/api/auth/signin';
+export const urlRegistration = 'https://84.228.171.29:3082/auth/api/auth/signup';
+export const profileName = 'https://84.228.171.29:3082/auth/api/auth/getProfileName';
+export const urlResetPassword = 'https://84.228.171.29:3082/auth/api/auth/user/resetPassword';
+export const urlChangePassword = 'https://84.228.171.29:3082/auth/api/auth/user/changePassword';
+
+export const authenticate = 'https://84.228.171.29:3082/auth/api/auth/authenticate';
 export const urlUploadingPhoto = 'https://api.cloudinary.com/v1_1/dachgyj58/auto/upload';
-export const urlSaveLostPet = 'https://192.168.31.100:3082/lostAndFound/saveLostPet';
-export const urlCardLostPets = 'https://192.168.31.100:3082/lostAndFound/getLostPets';
-export const urlGetLostPetById = 'https://192.168.31.100:3082/lostAndFound/getLostPetById';
-export const urlCardFoundPets = 'https://192.168.31.100:3082/lostAndFound/getFoundPets';
-export const urlSaveFoundPet = 'https://192.168.31.100:3082/lostAndFound/saveFoundPet';
-export const urlGetFoundPetById = 'https://192.168.31.100:3082/lostAndFound/getFoundPetById';
+export const urlSaveLostPet = 'https://84.228.171.29:3082/lostAndFound/saveLostPet';
+export const urlCardLostPets = 'https://84.228.171.29:3082/lostAndFound/getLostPets';
+export const urlGetLostPetById = 'https://84.228.171.29:3082/lostAndFound/getLostPetById';
+export const urlCardFoundPets = 'https://84.228.171.29:3082/lostAndFound/getFoundPets';
+export const urlSaveFoundPet = 'https://84.228.171.29:3082/lostAndFound/saveFoundPet';
+export const urlGetFoundPetById = 'https://84.228.171.29:3082/lostAndFound/getFoundPetById';
 
-export const urlGetPosts = 'https://192.168.31.100:3082/lostAndFound/getPosts';
-export const urlSavePost = 'https://192.168.31.100:3082/lostAndFound/savePost';
+export const urlGetPosts = 'https://84.228.171.29:3082/lostAndFound/getPosts';
+export const urlSavePost = 'https://84.228.171.29:3082/lostAndFound/savePost';
 
-export const urlGetFavorites = 'https://192.168.31.100:3082/lostAndFound/getFavorites';
-export const urlSaveFavorite = 'https://192.168.31.100:3082/lostAndFound/saveFavoritePost';
-export const urlDeleteFavorite = 'https://192.168.31.100:3082/lostAndFound/favorites';
-
-/*export const urlCardPostHotels = 'http://192.168.31.100:3082/lostAndFound/getFoundPets';
-export const urlSavePostHotels = 'http://192.168.31.100:3082/lostAndFound/saveFoundPet';
-
-export const urlCardPostWalking = 'http://192.168.31.100:3082/lostAndFound/getFoundPets';
-export const urlSavePostWalking = 'http://192.168.31.100:3082/lostAndFound/saveFoundPet';
-
-export const urlCardPostFostering = 'http://192.168.31.100:3082/lostAndFound/getFoundPets';
-export const urlSavePostFostering = 'http://192.168.31.100:3082/lostAndFound/saveFoundPet';
-
-export const urlCardPostVetHelp = 'http://192.168.31.100:3082/lostAndFound/getFoundPets';
-export const urlSavePostVetHelp = 'http://192.168.31.100:3082/lostAndFound/saveFoundPet';
-
-export const urlCardPostCare = 'http://192.168.31.100:3082/lostAndFound/getFoundPets';
-export const urlSavePostCare = 'http://192.168.31.100:3082/lostAndFound/saveFoundPet';*/
+export const urlGetFavorites = 'https://84.228.171.29:3082/lostAndFound/getFavorites';
+export const urlSaveFavorite = 'https://84.228.171.29:3082/lostAndFound/saveFavoritePost';
+export const urlDeleteFavorite = 'https://84.228.171.29:3082/lostAndFound/favorites';
 
 export const CLOUD_NAME = 'dachgyj58';
 export const CLOUD_UPLOAD_PRESET = 'photoLi';
-
-/*export const urlLogin = 'https://propets-auth-service.herokuapp.com/api/auth/signin';
-export const urlRegistration = 'https://propets-auth-service.herokuapp.com/api/auth/signup';
-export const profileName = 'https://propets-auth-service.herokuapp.com/api/auth/getProfileName';
-export const urlResetPassword = 'https://propets-auth-service.herokuapp.com/api/auth/user/resetPassword';
-export const urlChangePassword = 'https://propets-auth-service.herokuapp.com/api/auth/user/changePassword';
-/!*export const localChangePassword = 'http://192.168.31.25:8080/api/auth/user/changePassword';*!/
-export const authenticate = 'https://propets-auth-service.herokuapp.com/api/auth/authenticate';
-export const urlUploadingPhoto = 'https://api.cloudinary.com/v1_1/dachgyj58/auto/upload';*/
-/*export const urlSaveLostPet = 'https://propets-lostandfoundservice.herokuapp.com/saveLostPet';  //разбудит lostandfoundservice*/
-
-/*
-accessToken = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsaW5ldHNrSSIsImlhdCI6MTYxNzE0Mjk5OSwiZXhwIjoxNjE3MjI5Mzk5fQ.e4tsYk_MJVaEu3MDrSrvGmt5lHKftUXFyHFX7d4p4VqNLKogDQb6rVk5U9J8Ljj-hqPe4lD3-QiRvQLGVBcIPQ';
-tokenType: "Bearer" */
-
-/*
-//from cloudinary
-export default {
-    cloud_name: 'dachgyj58',
-    upload_preset: 'qkr2cecu' //Create an unsigned upload preset and update this
-};
-export const PHOTOS_FETCHED = 'PHOTOS_FETCHED';
-export const PHOTOS_UPLOADED = 'PHOTOS_UPLOADED';
-export const DELETE_UPLOADED_PHOTO = 'DELETE_UPLOADED_PHOTO';
-export const UPDATE_UPLOADED_PHOTO = 'UPDATE_UPLOADED_PHOTO';*/
-
-/*export const CLOUD_API_KEY = '371978636849623';*/
-/*export const CLOUD_API_SECRET = 'eEEni1GWZ-2x-bPqgf1hiRHPwfE';*/
-
-/*const cloudinary = require("cloudinary").v2;
-
-export default async (_req, res) => {
-    res.statusCode = 200;
-    res.json({ signature: 'photo', timestamp: 12345 });
-};*/
-
-/*export const miniCard = 'MiniCard';
-export const cardLost = 'CardLost';*/
